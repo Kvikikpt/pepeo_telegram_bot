48bd06526ca858609b640a0c86e1bb98af7662c6
Язиков Степан <cnikia@iMac-admin.local>
2020-02-08 16:57:33 +0300

    pepeo 1.0

--- bot.py
+++ bot.py
@@ -7,10 +7,14 @@ import final
 #pip install pyTelegramBotAPI
 #pip install requests
 #pip install Pillow
+#pip install bs4
+#pip install lxml
 
 from telebot import types
+from bs4 import BeautifulSoup
 
 bot = telebot.TeleBot(config.TOKEN)
+USERS = set()
 
 
 @bot.message_handler(commands=['start'])
@@ -30,10 +34,14 @@ def welcome(message):
                      parse_mode='html', reply_markup=markup)
 
 
-@bot.message_handler(commands=['serfnet'])
+@bot.message_handler(commands=['help'])
 def serf(message):
 	if message.chat.type == 'private':
-		bot.send_message(message.chat.id, 'Пожалуйста, дай мне ссылку! Oink.')
+		t = 'Тук'
+		b = 'Рандомное число'
+		c = 'Как дела?'
+		d = 'http://'
+		bot.send_message(message.chat.id, f'Команда \'{t}\' - специальный скрипт для тренировки барабанщиков.\n Введите \'{t}\' и вам отправят случайную барабанную парчу. \n \'{b}\' - генератор рандомных чисел.\n \'{c}\' - с помощью этой команды можно узнать настроение бота.\n Так же у бота есть функция выгрузки информации с интернет страницы. Для этого просто нужно отправить боту любую ссылку в формате ')
 
 
 
@@ -43,11 +51,32 @@ def lalala(message):
         if message.text == 'Рандомное число':
             bot.send_message(message.chat.id, f"{str(random.randint(0, 100))} Oink.")
         elif message.text == 'Как дела?':
-            bot.send_message(message.chat.id, 'Я мертв в душе! Oink...')
+        	i = random.randint(1, 2)
+        	if i == 1:
+        		sti = open('stickers/bad.webp', 'rb')
+        		bot.send_sticker(message.chat.id, sti)
+        		bot.send_message(message.chat.id, 'Я мертв в душе! Oink...')
+        	elif i == 2:
+        		sti = open('stickers/ok.webp', 'rb')
+        		bot.send_sticker(message.chat.id, sti)
+        		bot.send_message(message.chat.id, 'Все ок)) Oink!')
         elif message.text == 'Тук':
             final.photo_select()
             with open('someshet.jpg', 'rb') as ph:
             	bot.send_photo(message.from_user.id, photo=ph)
+        elif 'www' in message.text or 'http' in message.text:
+        	rs = requests.get(message.text)
+        	if rs.status_code == 200:
+        		root = BeautifulSoup(rs.content, 'html.parser')
+        		article = root.select_one('article')
+        		sti = open('stickers/rdy.webp', 'rb')
+        		bot.send_message(message.chat.id, f'{article.text}')
+        		bot.send_sticker(message.chat.id, sti)
+        		bot.send_message(message.chat.id, 'Oink!')
+        	else:
+        		sti = open('stickers/nrdy.webp', 'rb')
+        		bot.send_sticker(message.chat.id, sti)
+        		bot.send_message(message.chat.id, 'An error occured. Oink. Maybe you wrote a wrong link? Oink.')
         else:
             sti = open('stickers/wha.webp', 'rb')
             bot.send_sticker(message.chat.id, sti)
